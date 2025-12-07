3e641b5f02abfa28f06674456b3158d2c8e4d043
Cylerss <mmuralikrishna2007@gmail.com>
2025-12-07 16:36:53 +0530

    Deployment Configurations

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,8 @@
 import express from 'express';
 import {ENV} from './lib/env.js';
+import path from 'path';
 
-
+const __dirname=path.resolve();
 
 const app = express();
 const PORT = ENV.PORT || 3000;
@@ -9,6 +10,18 @@ const PORT = ENV.PORT || 3000;
 app.get('/', (req, res) => {
   res.send('Hello, World!');
 });
+
+
+
+
+if (ENV.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../frontend/dist')));
+
+  app.get("/{*any}", (req, res) => {
+    res.sendFile(path.join(__dirname, '../frontend','dist','index.html'));
+    }
+  );
+}
 
 
 app.listen(PORT, () => {
